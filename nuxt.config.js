23b6dfa0b667d5b1871080098a76ee45ee88d2d4
Federico Armellini <armellini.federico@gmail.com>
2021-08-17 00:32:02 +0200

    fix build

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,9 +58,17 @@ export default {
     // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {
         babel: {
-            plugins: [
-                ['@babel/plugin-proposal-private-methods', { loose: true }]
+          presets({isServer}) {
+            const targets = isServer ? { node: 'current' } : { ie: 11 }
+            return [
+              [ require.resolve("@babel/preset-env"), { targets }  ]
             ]
+          },
+          plugins: [
+            "@babel/syntax-dynamic-import",
+            "@babel/transform-runtime",
+            "@babel/transform-async-to-generator"
+          ]
         }
     }
 }
