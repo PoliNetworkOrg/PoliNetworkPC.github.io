6a5885a133f1ebc89e6804adaee38406504b6e97
Federico Armellini <armellini.federico@gmail.com>
2021-08-17 12:27:17 +0200

    title change

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: 'PoliNetworkElezioni',
+        title: 'PoliNetworkPC 💻',
         htmlAttrs: {
             lang: 'en'
         },
