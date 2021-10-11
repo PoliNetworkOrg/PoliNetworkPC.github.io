443c231e54730aa53552356bd24cace204724806
ArmeF97 <ArmeF97@users.noreply.github.com>
2021-10-11 13:20:20 +0000

    deploy: d22b4718cc0cdaabc16b117c99430548e114c4c3

--- _nuxt/static/1633958388/en/trei_corsi/matematica/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633958388",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Ftrei_corsi\u002Fmatematica",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
