50aade5f1639197090dd6de898f768a5f57291e1
ArmeF97 <ArmeF97@users.noreply.github.com>
2021-10-26 08:34:59 +0000

    deploy: 30e88eff8723cd1a194652d99f4f5c6cf7b89c50

---
+++ _nuxt/static/1635237289/it/trei_corsi/informatica/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635237289",layout:"default",error:a,serverRendered:true,routePath:"\u002Fit\u002Ftrei_corsi\u002Finformatica",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
