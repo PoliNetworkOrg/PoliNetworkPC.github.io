32b6a41d57295ea032c01f243f769802d1472791
ArmeF97 <ArmeF97@users.noreply.github.com>
2021-11-19 14:37:05 +0000

    deploy: ebc83ea9950710bd3e0a541f248a5fa12aac61ce

---
+++ _nuxt/static/1637332617/trei_corsi/default/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637332617",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftrei_corsi\u002Fdefault",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
