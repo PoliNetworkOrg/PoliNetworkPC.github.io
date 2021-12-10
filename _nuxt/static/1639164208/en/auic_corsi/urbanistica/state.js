81e1daf5ff05d043012f7aa2611dfc5e473e31bf
ArmeF97 <ArmeF97@users.noreply.github.com>
2021-12-10 19:25:57 +0000

    deploy: 99791ef2f0492f1797043311480ba8dbfaebbd01

--- _nuxt/static/1639164208/en/auic_corsi/urbanistica/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639164208",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fauic_corsi\u002Furbanistica",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
