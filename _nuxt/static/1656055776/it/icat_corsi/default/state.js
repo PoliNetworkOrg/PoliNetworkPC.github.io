e37d9d1737109102d187994a81a296bb01e35c09
steven9463912 <steven9463912@users.noreply.github.com>
2022-06-28 09:21:51 +0000

    deploy: c86eb1d511c7e7d96b93f8ea8f20bee1d8e6e766

--- _nuxt/static/1656055776/it/icat_corsi/default/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656055776",layout:"default",error:a,serverRendered:true,routePath:"\u002Fit\u002Ficat_corsi\u002Fdefault",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
