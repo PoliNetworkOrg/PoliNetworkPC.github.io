5bc1de6a2621108cf75aba242076d0f61ca15ad0
steven9463912 <steven9463912@users.noreply.github.com>
2022-07-23 12:02:58 +0000

    deploy: ab2a02a2ac5c1ff9270e778604a83d7874f73757

---
+++ _nuxt/static/1658577772/it/trei_corsi/default/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1658577772",layout:"default",error:a,serverRendered:true,routePath:"\u002Fit\u002Ftrei_corsi\u002Fdefault",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
