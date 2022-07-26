7e3b5ca4fdcab217aef22da3c18e0d79f9027389
steven9463912 <steven9463912@users.noreply.github.com>
2022-07-26 15:19:53 +0000

    deploy: fe367267c2f7845dbc39b6956aeed6cf929a9b52

--- _nuxt/default-1dc7678b.mjs
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./PCSingolo-16363d18.mjs";import{a as n,o as a,b as s,f as r}from"./entry-43c4c961.mjs";const c={data(){return{items:{notFound:!0,scuolalink:"../../design/",buttons:[]}}}};function _(i,l,u,d,t,p){const e=o;return a(),s("div",null,[r(e,{"data-lista":t.items,language:"it"},null,8,["data-lista"])])}var g=n(c,[["render",_]]);export{g as default};
---
+++ _nuxt/default-fa674a9f.mjs
@@ -0,0 +1,1 @@
+import{_ as o}from"./PCSingolo-2b0120d7.mjs";import{a as n,o as a,b as s,f as r}from"./entry-ba262195.mjs";const c={data(){return{items:{notFound:!0,scuolalink:"../../design/",buttons:[]}}}};function _(i,l,u,d,t,p){const e=o;return a(),s("div",null,[r(e,{"data-lista":t.items,language:"it"},null,8,["data-lista"])])}var g=n(c,[["render",_]]);export{g as default};
