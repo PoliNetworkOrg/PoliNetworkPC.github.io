fe367267c2f7845dbc39b6956aeed6cf929a9b52
user <user@example.com>
2022-07-26 17:18:46 +0200

    Update en.js, en.json, and 5 more files...

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,53 +1,63 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-	target: 'static',
+	target: "static",
 	head: {
-		title: 'PoliNetworkPC 💻',
+		title: "PoliNetworkPC 💻",
 		htmlAttrs: {
-			lang: 'en'
+			lang: "en",
 		},
 		meta: [
-			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: '' }
+			{ charset: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{ hid: "description", name: "description", content: "" },
 		],
-		link: [
-			{ rel: 'icon', type: 'image/x-icon', href: '/static/img/icon.png' }
-		],
+		link: [{ rel: "icon", type: "image/x-icon", href: "/static/img/icon.png" }],
 		script: [
 			{
 				src: "/jquery/jquery.min.js",
-				type: "text/javascript"
+				type: "text/javascript",
 			},
 			{
 				src: "/js/bootstrap.bundle.min.js",
-				type: "text/javascript"
+				type: "text/javascript",
 			},
 			{
 				src: "/js/custom.js",
-				type: "text/javascript"
+				type: "text/javascript",
 			},
-		]
+		],
 	},
-	modules: ['@intlify/nuxt3'],
-	intlify: {
-		vueI18n:{},
-		localeDir: 'locales', 
+	modules: [ "@nuxtjs/i18n"],
+	i18n: {
+		strategy: "prefix",
+		langDir: "locales/",
+		locales: [
+			{
+				code: "en",
+				iso: "en-US",
+				file: "en.js",
+				isCatchallLocale: true,
+			},
+			{
+				code: "it",
+				iso: "it-IT",
+				file: "it.js",
+			},
+		],
+		defaultLocale: "it",
+		vueI18n: {
+			fallbackLocale: "it",
+		},
+	},
 
-	  },
-
-
-	  css: [
-		'~/assets/bootstrap/bootstrap.css',
-		'~/assets/bootstrap/style.css',
-		'~/assets/css/style.css'
+	css: [
+		"~/assets/bootstrap/bootstrap.css",
+		"~/assets/bootstrap/style.css",
+		"~/assets/css/style.css",
 	],
 
-	  // Auto import components: https://go.nuxtjs.dev/config-components
-	  components: true,
-
-
-})
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
+});
