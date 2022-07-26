afc09838ba93caccddae3fd7995da64e2358ee89
user <user@example.com>
2022-07-26 11:28:40 +0200

    Update nuxt.config.js

--- PoliNetworkPC/nuxt.config.js
+++ PoliNetworkPC/nuxt.config.js
@@ -37,6 +37,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
+	'@nuxtjs/i18n',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
