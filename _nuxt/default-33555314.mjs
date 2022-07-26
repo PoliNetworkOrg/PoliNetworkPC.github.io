7cd46a2c512a4e94f1900eeedef88b171eac48a1
steven9463912 <steven9463912@users.noreply.github.com>
2022-07-26 15:49:17 +0000

    deploy: a7bd2dc5205d15981be87a531baccd639926a0db

---
+++ _nuxt/default-33555314.mjs
@@ -0,0 +1,1 @@
+import{_ as o}from"./PCSingolo-71b90850.mjs";import{a as n,o as a,b as s,f as r}from"./entry-97d8f61b.mjs";const c={data(){return{items:{notFound:!0,scuolalink:"../../design/",buttons:[]}}}};function _(i,l,u,d,t,p){const e=o;return a(),s("div",null,[r(e,{"data-lista":t.items,language:"it"},null,8,["data-lista"])])}var g=n(c,[["render",_]]);export{g as default};
