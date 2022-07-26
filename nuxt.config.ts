7883ef7b3831fa5fb88abde19e359436e14afbc4
user <user@example.com>
2022-07-26 15:42:57 +0200

    Update .editorconfig, .editorconfig, and 350 more files...

---
+++ nuxt.config.ts
@@ -0,0 +1,53 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+	target: 'static',
+	head: {
+		title: 'PoliNetworkPC 💻',
+		htmlAttrs: {
+			lang: 'en'
+		},
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/img/icon.png' }
+		],
+		script: [
+			{
+				src: "/jquery/jquery.min.js",
+				type: "text/javascript"
+			},
+			{
+				src: "/js/bootstrap.bundle.min.js",
+				type: "text/javascript"
+			},
+			{
+				src: "/js/custom.js",
+				type: "text/javascript"
+			},
+		]
+	},
+	modules: [ '@intlify/nuxt3'],
+	intlify: {
+		
+		localeDir: 'locales/', 
+
+	  },
+
+
+	  css: [
+		'~/assets/bootstrap/bootstrap.css',
+		'~/assets/bootstrap/style.css',
+		'~/assets/css/style.css'
+	],
+
+	  // Auto import components: https://go.nuxtjs.dev/config-components
+	  components: true,
+
+	
+
+})
